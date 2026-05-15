40d0fc90707eac9653b5c7ebd040f7b0953bdd48
Renata Masini <masini@gmail.com>
2026-05-15 16:04:28 +0100

    added js glider

---
+++ js/quotes.js
@@ -0,0 +1,10 @@
+let glideOptions = {
+  type: "carousel",
+  startAt: 0,
+  perView: 1,
+  autoplay: 3000,
+  animationDuration: 2000,
+  hoverpause: true,
+};
+
+new Glide(".glide", glideOptions).mount();
