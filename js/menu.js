51d781ec0a01a901a9015b276963f50cfe37e839
Renata Masini <masini@gmail.com>
2026-05-29 12:06:08 +0100

    menu added

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", function () {
+  // grab a reference to all the elements that will control the show/hide of the nav
+
+  let menuButton = document.querySelector("#menu-btn");
+  let menu = document.querySelector(".menu");
+  // const closeButton = document.querySelector('#close-button');
+
+  // loop through each of the nav icon triggers and add an event listener
+
+  menuButton.addEventListener("click", toggleMenu);
+
+  // closeButton.addEventListener('click', toggleMenu);
+
+  // create the event handler function to toggle the active state of body
+
+  function toggleMenu() {
+    // document.body.classList.toggle("menu-is-open");
+    menu.style.display = menu.style.display === "none" ? "block" : "none";
+  }
+});
